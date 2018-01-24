34deed82630adc736e0aa53285e5f0e367fc8fcd
Mayank Garg <mayankg567@gmail.com>
2018-01-24 12:36:38 +0530

    user model and hbm file

---
+++ src/com/vpool/model/UserModel.java
@@ -0,0 +1,148 @@
+package com.vpool.model;
+
+import java.io.Serializable;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserModel extends ActionSupport implements Serializable {
+	private int userID, pincode;
+	private String emailID, pass, firstName, middleName, lastName, mobNo, altMobNo, gender, address, dob;
+	private String city, state, status, photoLoc, registeredOn;
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+	public String getEmailID() {
+		return emailID;
+	}
+
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMobNo() {
+		return mobNo;
+	}
+
+	public void setMobNo(String mobNo) {
+		this.mobNo = mobNo;
+	}
+
+	public String getAltMobNo() {
+		return altMobNo;
+	}
+
+	public void setAltMobNo(String altMobNo) {
+		this.altMobNo = altMobNo;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getPhotoLoc() {
+		return photoLoc;
+	}
+
+	public void setPhotoLoc(String photoLoc) {
+		this.photoLoc = photoLoc;
+	}
+
+	public String getRegisteredOn() {
+		return registeredOn;
+	}
+
+	public void setRegisteredOn(String registeredOn) {
+		this.registeredOn = registeredOn;
+	}
+
+}
