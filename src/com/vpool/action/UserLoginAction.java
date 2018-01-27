ed39ca975e2326de47c9cc4732a1312942ef6565
Mayank Garg <mayankg567@gmail.com>
2018-01-28 00:35:30 +0530

    login success

--- src/com/vpool/action/UserLoginAction.java
+++ src/com/vpool/action/UserLoginAction.java
@@ -9,12 +9,11 @@ import org.apache.struts2.interceptor.ServletRequestAware;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
-import com.opensymphony.xwork2.ActionSupport;
 import com.vpool.middle.EncryptText;
 import com.vpool.model.UserModel;
 import com.vpool.util.HBUtil;
 
-public class UserLoginAction extends ActionSupport implements ServletRequestAware {
+public class UserLoginAction extends UserModel implements ServletRequestAware {
 	HttpServletRequest request;
 	HttpSession session;
 
@@ -27,22 +26,26 @@ public class UserLoginAction extends ActionSupport implements ServletRequestAwar
 			String pass = request.getParameter("pass");
 			String encpass = new EncryptText().getMD5(pass);
 			Session hbSession = HBUtil.openSession();
-			String qry = "from UserModel where emailID=?";
+			String qry = "from UserModel where emailID=:emID and pass=:pass";
 			Query q1 = hbSession.createQuery(qry);
 			System.out.println("create query success");
-			q1.setString(0, email);
-			List userList = q1.list();
 
-			if (userList.isEmpty()) {
-				System.out.println("record not found");
-				HBUtil.closeSF();
-				return "error";
-			} else {
+			q1.setString("emID", email);
+			q1.setString("pass", encpass);
+
+			List userList = q1.list();
+			System.out.println("list size:" + userList.size());
+			if (userList != null) {
 				UserModel um1 = (UserModel) userList.get(0);
 				System.out.println("user found");
 				System.out.println(um1.getFirstName());
 				HBUtil.closeSF();
 				return "success";
+
+			} else {
+				System.out.println("record not found");
+				HBUtil.closeSF();
+				return "error";
 			}
 
 		} catch (Exception e) {
